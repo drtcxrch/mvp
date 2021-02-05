d7bf5fbc1dd0b61abedd44373f6b49e11021fb0f
Grant Taggart <granttaggartsf@gmail.com>
2021-02-05 10:04:36 -0800

    Complete routes

---
+++ server/routes/bike-router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const BikeCtrl = require('../controllers/bike-ctrl');
+
+const router = express.Router();
+
+router.post('/bike', BikeCtrl.createBike);
+router.put('/bike/:id', BikeCtrl.updateBike);
+router.delete('/bike/:id', BikeCtrl.deleteBike);
+router.get('/bike/:id', BikeCtrl.getMovieById);
+router.get('/bikes', BikeCtrl.getBikes);
+
+module.exports = router;
